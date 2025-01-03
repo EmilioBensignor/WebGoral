7c8a2a523d45ce8dd7100a348fd5764022537e63
Emilio Bensignor <liobensignor@gmail.com>
2025-01-03 13:27:49 -0300

    Configuracion de Modulos + Routes names + Layout + run start + css generico

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="layout">
     <DefaultHeader />
     <slot />
     <DefaultFooter />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,18 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxt/icon",
     '@nuxtjs/seo',
+    'nuxt-vitalizer',
+    'nuxt-booster',
+    '@nuxtjs/fontaine',
+    'nuxt-anchorscroll',
+    '@nuxt/scripts',
+    '@nuxt/fonts',
   ],
+  fonts: {
+    defaults: {
+      weights: [400, 500, 700, 900],
+    }
+  },
   icon: {
     size: '1rem',
     serverBundle: {
@@ -22,25 +33,52 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://unpkg.com/primeflex@latest/primeflex.css'
         },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com'
-        },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com'
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap'
-        },
       ],
     }
   },
   site: {
-    url: 'https://virtual.iconicworkspaces.com',
-    name: 'Iconic Virtual Offices',
-    description: 'Iconic Virtual Offices',
-    defaultLocale: 'en',
+    url: 'https://goral.com',
+    name: 'Goral',
+    description: 'Goral',
+    defaultLocale: 'es',
+  },
+  booster: {
+    detection: {
+      performance: true,
+      browserSupport: true,
+      battery: true
+    },
+    performanceMetrics: {
+      timing: {
+        fcp: 800,
+        dcl: 1200
+      }
+    },
+    lazyOffset: {
+      component: '0%',
+      asset: '0%'
+    }
+  },
+  image: {
+    screens: {
+      default: 320,
+      xxs: 480,
+      xs: 576,
+      sm: 768,
+      md: 996,
+      lg: 1200,
+      xl: 1367,
+      xxl: 1600,
+      '4k': 1921
+    },
+    domains: ['img.youtube.com', 'i.vimeocdn.com'],
+    alias: {
+      youtube: 'https://img.youtube.com',
+      vimeo: 'https://i.vimeocdn.com',
+    }
+  },
+  vitalizer: {
+    disableStylesheets: 'entry',
+    disablePrefetchLinks: true
   },
 })
